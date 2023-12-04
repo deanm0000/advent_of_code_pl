8e46fba7705f9e6a2ba82ee4dfb92506cda30cc7
deanm0000 <powertrading121@gmail.com>
2023-12-04 18:33:46 +0000

    commenting

--- day3.py
+++ day3.py
@@ -1,10 +1,24 @@
 import polars as pl
 
+### Part One
+
+# The engineer explains that an engine part seems to be missing from the engine,
+# but nobody can figure out which one. If you can add up all the part numbers in
+# the engine schematic, it should be easy to work out which part is missing.
+
+# The engine schematic (your puzzle input) consists of a visual representation
+# of the engine. There are lots of numbers and symbols you don't really understand,
+# but apparently any number adjacent to a symbol, even diagonally, is a "part number"
+# and should be included in your sum. (Periods (.) do not count as a symbol.)
+
+
 with open("day3_input.txt") as ff:
     intxt = ff.read()
 
 df = pl.DataFrame({"a": intxt.split("\n")})
 
+
+# Split the rows into each individual character and map it by its row/column position
 df = (
     df.with_columns(
         pl.col("a").str.split("").list.gather(pl.int_range(1, pl.count() + 1)),
@@ -14,6 +28,9 @@ df = (
     .explode("a", "c")
 )
 
+
+# Make DF of just the numbers with begin and end column position within each row but
+# also keep an entry for every column position the number takes up
 nums = (
     df.with_columns(
         is_num=pl.col("a").str.extract("(\d)").is_not_null(),
@@ -36,6 +53,8 @@ nums = (
     .with_columns(pl.col("r").cast(pl.Int32), pl.col("c").cast(pl.Int32))
 )
 
+# Make a DF of just the non . symbols and then copy each one's r/c position to be
+# r=r-1, r, r+1 and c=c-1, c, c+1 to capture the diagonals
 symbols = (
     df.filter((pl.col("a") != ".") & (pl.col("a").str.extract("(\d)").is_null()))
     .with_columns(
@@ -47,7 +66,8 @@ symbols = (
     .with_columns(pl.col("r").cast(pl.Int32), pl.col("c").cast(pl.Int32))
 )
 
-
+# Join the two DFs by their r/c position and then only keep unique by r, min_c, max_c. The sum of the
+# a_right column is the final answer
 part_one_ans = (
     symbols.join(nums, on=["r", "c"]).unique(["r", "min_c", "max_c"])["a_right"].sum()
 )
@@ -55,6 +75,16 @@ part_one_ans = (
 
 ### Part two
 
+# The missing part wasn't the only issue - one of the gears in the engine is wrong. A gear is any *
+# symbol that is adjacent to exactly two part numbers. Its gear ratio is the result of multiplying
+# those two numbers together.
+
+# This time, you need to find the gear ratio of every gear and add them all up so that the engineer
+# can figure out which gear needs to be replaced.
+
+
+# Make a DF of just * the same way as symbols above. This time maintain the original position
+# of each asterisk so it can be grouped by later.
 asterisks = (
     df.filter(pl.col("a") == "*")
     .with_columns(
@@ -68,8 +98,11 @@ asterisks = (
     .with_columns(pl.col("r").cast(pl.Int32), pl.col("c").cast(pl.Int32))
 )
 
+# Just like in part 1, join by r/c and take unique but then another step follows
 joined = asterisks.join(nums, on=["r", "c"]).unique(["r", "min_c", "max_c"])
 
+# Filter the join by the count for each symbol (denoted by original positions) and
+# do grouping product and sum the series for final answer
 part_two_ans = (
     joined.filter(pl.count().over(["orig_r", "orig_c"]) == 2)
     .sort(["orig_r", "orig_c"])
