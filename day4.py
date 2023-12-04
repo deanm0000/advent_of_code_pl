dfe5629087124243c0d9ebbdceb5998ca5258dc2
deanm0000 <powertrading121@gmail.com>
2023-12-04 18:18:15 +0000

    numba solution

--- day4.py
+++ day4.py
@@ -61,32 +61,60 @@ wins = (
     )
     .select("Card_num", wins=pl.col("winning").list.set_intersection("have").list.len())
 )
-# max_wins=wins['wins'].max()
 
-# wins.select('Card_num', 'wins', lookback0=pl.lit(1),
+
+# Brainstorming a not looping way to get answer
+# but doesn't work
+# max_wins=wins['wins'].max()
+# lookback=(
+# wins
+# .select('Card_num', 'wins', lookback0=pl.lit(1),
 #     **{f"lookback{x}": (
 #     pl.when(pl.col('wins').shift(x)>=x)
 #     .then(pl.lit(1))
 #     .otherwise(pl.lit(0))) for x in range(1,1+max_wins)
 #                      })
+# )
 
-
-card_range = wins["Card_num"].unique()
+ans_wins = wins
+card_range = ans_wins["Card_num"].unique()
 for card in card_range:
     # Add copies of cards
-    cur_card_wins = wins.filter(pl.col("Card_num") == card)["wins"][0]
+    cur_card_wins = ans_wins.filter(pl.col("Card_num") == card)["wins"][0]
     if cur_card_wins == 0:
         next
-    card_copies = wins.filter(
+    card_copies = ans_wins.filter(
         pl.col("Card_num").is_between(card + 1, card + cur_card_wins)
     ).unique()
-    wins = pl.concat(
+    ans_wins = pl.concat(
         [
-            wins,
+            ans_wins,
             *[
                 card_copies
-                for _ in range(wins.filter(pl.col("Card_num") == card).shape[0])
+                for _ in range(ans_wins.filter(pl.col("Card_num") == card).shape[0])
             ],
         ]
     )
-part2_ans = wins.shape[0]
+part2_ans = ans_wins.shape[0]
+## This takes about 16.5s
+# 18846301
+
+### Numba
+import numba as nb
+import numpy as np
+
+
+@nb.guvectorize([(nb.int32[:], nb.int32[:])], "(n)->(n)", nopython=True)
+def cum_cards(wins, res):
+    res[:] = np.repeat(1, wins.shape[0])
+    for card in range(wins.shape[0]):
+        for i in range(wins[card]):
+            true_i = card + i + 1
+            res[true_i] = res[true_i] + res[card]
+
+
+part2_nb_ans = wins.with_columns(haves=cum_cards(pl.col("wins").cast(pl.Int32)))[
+    "haves"
+].sum()
+## This takes about 0s
+# 18846301
